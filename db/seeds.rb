9fba85f5a3f658282e03bc65c872045f56419466
gunjx <tommy.halks@gmail.com>
2018-03-06 18:25:45 +0100

    Use a database migration to add a price column to products table. Create multiple products from db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Guarana", description: "Guarana is a climbing plant native to the Amazon basin and especially common in Brazil. As a dietary supplement, guarana is an effective stimulant: Their seeds are naturally high in caffeine. Guarana can be used as an energising, stimulating alternative to coffee and sugary energy drinks.", image_url: "https://images-na.ssl-images-amazon.com/images/I/91VFdOnJnEL._SL1500_.jpg", price: "44.50")
+
+Product.create(name: "Tapioca", description: "Tapioca is made from starch extracted from the roots of the cassava plant. The starch is formed into pearls, making the perfect base for tapioca pudding, boba (bubble) tea, the Indian dish sabudana khichdi, and vegan 'caviar.' Small Pearl Tapioca is also an excellent addtion to creamy soups or dumplings.", image_url: "https://images-na.ssl-images-amazon.com/images/I/716DtvQWwXL._SL1156_.jpg", price: "8.97")
+
+Product.create(name: "Matcha", description: "Matcha is all-natural and created from premium quality tea plants. It is 100% organic with no added artificial ingredients. It uses all-natural flavors to ensure that its natural essence is sustained.", image_url: "https://images-na.ssl-images-amazon.com/images/I/61vcvPDbNhL._SL1000_.jpg", price: "15.73")
+
+Product.create(name: "Matcha", description: "Buckwheat is a plant related to rhubarb and its groats are a gluten free seed. This fun 'grain' is shaped like a pyramid and is a complete protein, containing all 9 of the essential amino acids. It can be steamed, toasted, roasted, ground into flour, or boiled. Try Buckwheat Groats in any recipe that calls for whole grains: pilafs, salads, casseroles, soups, granolas, stews, or as a cereal on its own. Buckwheat groats are also the key ingredient in traditional Eastern European dishes like Kasha Varnishkes and Potato Knishes.", image_url: "https://images-na.ssl-images-amazon.com/images/I/81kYus7MzWL._SL1500_.jpg", price: "9.66")
