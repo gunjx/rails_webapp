a0832f55652158722a93b450cb72e9e849ff697e
gunjx <tommy.halks@gmail.com>
2018-03-13 10:52:24 +0100

    Remove debugging code

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,7 +3,6 @@ class PaymentsController < ApplicationController
   def create
     token = params[:stripeToken]
     @product = Product.find(params[:product_id])
-    byebug
     if user_signed_in?
       @user = current_user
     else
