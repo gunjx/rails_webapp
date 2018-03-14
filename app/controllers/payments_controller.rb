b49e08b626121a2e159b50a76774b05ec98f8be2
gunjx <tommy.halks@gmail.com>
2018-03-14 23:13:13 +0100

    Fix redirect unauthorized users before creating orders. Fix flash messages: alert/warning, notice/info, error/danger.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,32 +7,35 @@ class PaymentsController < ApplicationController
       @user = current_user
     else
       redirect_to new_user_registration_path
+      flash[:warning] = t("devise.failure.unauthenticated")
     end
 
-    begin
-      charge = Stripe::Charge.create(
-        amount: (@product.price * 100).to_i,
-        currency: 'eur',
-        source: token,
-        description: params[:stripeEmail],
-        receipt_email: @user.email
-      )
-
-      if charge.paid
-        Order.create(
-          product_id: @product.id,
-          user_id: @user.id,
-          total: @product.price
+    if @user
+      begin
+        charge = Stripe::Charge.create(
+          amount: (@product.price * 100).to_i,
+          currency: 'eur',
+          source: token,
+          description: params[:stripeEmail],
+          receipt_email: @user.email
         )
-        flash[:success] = 'Your payment was processed successfully'
+
+        if charge.paid
+          Order.create(
+            product_id: @product.id,
+            user_id: @user.id,
+            total: @product.price
+          )
+          flash[:success] = 'Your payment was processed successfully'
+        end
+
+      rescue Stripe::CardError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
       end
 
-    rescue Stripe::CardError => e
-      body = e.json_body
-      err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      redirect_to product_path(@product)
     end
-
-    redirect_to product_path(@product)
   end
 end
